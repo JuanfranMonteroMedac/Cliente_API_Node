1d0b8035da9dc4d90a60d75a950ac89b139a0dea
JuanfranMonteroMedac <jmt0003@alu.medac.es>
2022-01-26 10:40:09 +0100

    Finished vehicles features and working in services

--- assets/js/mainServicios.js
+++ assets/js/mainServicios.js
@@ -5,14 +5,14 @@ window.onload = () => {
         while(section.firstChild){
             section.removeChild(section.firstChild)
         }
-        let id_usuario = document.getElementById('id_usuario').value;
-        mostrarVehiculos(id_usuario)
+        let id_matricula = document.getElementById('id_matricula').value;
+        mostrarVehiculos(id_matricula)
     }
 } 
 
-function mostrarVehiculos(id_usuario){
+function mostrarVehiculos(id_matricula){
         
-        fetch(`http://localhost:3000/vehiclesId?id=${id_usuario}`,{
+        fetch(`http://localhost:3000/serviceVehicle?id=${id_matricula}`,{
             method: 'GET',
             mode: 'cors',
             headers: {
@@ -27,29 +27,19 @@ function mostrarVehiculos(id_usuario){
                 article.className = 'cardUsu'
                 for(let camposInternos in campos){
                         let p = document.createElement('p')
-                        let texto = ''
-                        if(camposInternos != 'año')  texto = document.createTextNode(`${camposInternos.toUpperCase()} : ${campos[camposInternos]}`)
-                        else{
-                            let fecha = new Date (campos[camposInternos]);
-                            texto = document.createTextNode(`${camposInternos.toUpperCase()} : ${fecha.toDateString()}`)
-                        }
+                        let texto = document.createTextNode(`${camposInternos.toUpperCase()} : ${campos[camposInternos]}`)
                         p.appendChild(texto)
                         article.appendChild(p)
                 }
                 let btn = document.createElement('input')
                         btn.type = 'button'
                         btn.className = 'buttonCardEdit'
-                        btn.value = 'Editar Vehiculo'
+                        btn.value = 'Editar Servicio'
                 article.appendChild(btn)
-                let btnServ = document.createElement('input')
-                    btnServ.type = 'button'
-                    btnServ.className = 'buttonCardVhc'
-                    btnServ.value = 'Ver Servicios'
-                article.appendChild(btnServ)
                 let btnDel = document.createElement('input')
                     btnDel.type = 'button'
                     btnDel.className = 'buttonCardDelete'
-                    btnDel.value = 'Elimincar Vehiculo'
+                    btnDel.value = 'Elimincar Servicio'
                 article.appendChild(btnDel)
                 document.querySelector('#container').appendChild(article)
             }
